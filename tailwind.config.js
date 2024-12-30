9485b9a2c16298e788b84cb1589ae0866c2f4493
k1fathi <k1fatghi33@gmail.com>
2024-12-31 00:26:29 +0300

    main button style is works fine

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         },
       },
       backgroundImage: {
-        "zuzzuu-gradient":
-          "linear-gradient(113deg, #29d7ff 18.07%, #3cbfff 27.42%, #7083ff 46.12%, #c123ff 73.51%, #e000ff 83.53%)",
+        "zuzzuu-gradient": "linear-gradient(113deg, #29d7ff 18.07%, #3cbfff 27.42%, #7083ff 46.12%, #c123ff 73.51%, #e000ff 83.53%)",
+        
       },
     },
   },
