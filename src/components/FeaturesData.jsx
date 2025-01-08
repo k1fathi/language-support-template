2d562c3bc5099ca4d861e01dcb3d4e3c96007c40
k1fathi <k1fatghi33@gmail.com>
2025-01-08 13:28:17 +0300

    fetures data components works fine

---
+++ src/components/FeaturesData.jsx
@@ -0,0 +1,19 @@
+// src/components/Features.js
+import React from "react";
+
+const FeaturesData = ({ features }) => {
+  return (
+    <section>
+      <div className="features-content">
+        {features.map((feature, index) => (
+          <div key={index}>
+            <h3 className="gradient-text-h2">{feature.title}</h3>
+            <p>{feature.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FeaturesData;
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,11 +1,25 @@
 // src/pages/Features.js
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FeaturesData from "../components/FeaturesData";
 import Features from "../components/Features";
 
-const FeaturesPage = () => (
-  <div>
-    <Features />
-  </div>
-);
+const FeaturesPage = () => {
+  const [features, setFeatures] = useState([]);
+
+  // Load data from the JSON file
+  useEffect(() => {
+    fetch("/data/features_data.json")
+      .then((response) => response.json())
+      .then((data) => setFeatures(data))
+      .catch((error) => console.error("Error loading features data:", error));
+  }, []);
+
+  return (
+    <div>
+      <Features/>
+      <FeaturesData features={features} />
+    </div>
+  );
+};
 
 export default FeaturesPage;
